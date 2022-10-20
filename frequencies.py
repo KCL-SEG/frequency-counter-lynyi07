212aa14d792f5394de9e691795570ed64592316b
Lyn Yi <Cheng Lyn Yi>
2022-10-20 14:53:05 +0100

    Done

--- frequencies.py
+++ frequencies.py
@@ -3,5 +3,11 @@
 
 def frequencies(items):
     frequencies = {}
-    # Your code goes here
+    for x in items: 
+        x_str= str(x)
+        if frequencies.get(x_str) == None: 
+            frequencies[x_str]=1;
+        else: 
+            frequencies[x_str] = frequencies.get(x_str)+1; 
+            # Your code goes here
     return frequencies
